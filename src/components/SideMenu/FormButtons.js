145a6d46ed5fd72e75a52a73ef4c93aa272a0e8a
mars <mars@mars.mars>
2021-10-26 16:21:31 -0400

    fixed FormHandler action

--- src/components/SideMenu/FormButtons.js
+++ src/components/SideMenu/FormButtons.js
@@ -28,60 +28,60 @@ const FormButtons = () => {
       > Email</Button>
       <Button
         className="Buttons"
-        onClick={() => buttonHandler("Email", "email")}
+        onClick={() => buttonHandler("Url", "url")}
         type="submit"
         size="sm"
       > Url</Button>
       <Button
         className="Buttons"
-        onClick={() => buttonHandler("Email", "email")}
+        onClick={() => buttonHandler("Phone number", "phone")}
         type="submit"
         size="sm"
       > Phone Number</Button>
       <Button
         className="Buttons"
-        onClick={() => buttonHandler("Email", "email")}
+        onClick={() => buttonHandler("Tags", "tags")}
         type="submit"
         size="sm"
       > Tags</Button>
       <Button
         className="Buttons"
-        onClick={() => buttonHandler("Email", "email")}
+        onClick={() => buttonHandler("Address", "address")}
         type="submit"
         size="sm"
       > Address</Button>
       <Button
         className="Buttons"
-        onClick={() => buttonHandler("Email", "email")}
+        onClick={() => buttonHandler("Data / Time", "data")}
         type="submit"
         size="sm"
       > Date / Time</Button>
       <Button
         className="Buttons"
-        onClick={() => buttonHandler("Email", "email")}
+        onClick={() => buttonHandler("Day", "day")}
         type="submit" size="sm"
       > Day</Button>
       <Button
         className="Buttons"
-        onClick={() => buttonHandler("Email", "email")}
+        onClick={() => buttonHandler("Time", "time")}
         type="submit"
         size="sm"
       > Time</Button>
       <Button
         className="Buttons"
-        onClick={() => buttonHandler("Email", "email")}
+        onClick={() => buttonHandler("Currency", "currency")}
         type="submit"
         size="sm"
       > Currency</Button>
       <Button
         className="Buttons"
-        onClick={() => buttonHandler("Email", "email")}
+        onClick={() => buttonHandler("Survey", "survey")}
         type="submit"
         size="sm"
       > Survey</Button>
       <Button
         className="Buttons"
-        onClick={() => buttonHandler("Email", "email")}
+        onClick={() => buttonHandler("Signature", "signature")}
         type="submit"
         size="sm"
         >Signature</Button>
